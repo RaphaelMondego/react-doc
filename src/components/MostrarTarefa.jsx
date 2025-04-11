cf7b8c77870cef3a5148c61b83cb3b9999fde3de
raphael.mondego <raphamondego@gmail.com>
2025-04-11 17:53:11 -0300

    alterações no código da revisão

--- src/components/MostrarTarefa.jsx
+++ src/components/MostrarTarefa.jsx
@@ -2,17 +2,31 @@ import { useState } from "react";
 
 function Tarefas(){
     const [tarefa, setTarefa] = useState("");
+    const [listaTarefas, setListaTarefas] = useState([]);
 
-    function mudarTarefa(event){
-        setTarefa(event.target.value);
+    function adicionarTarefa(){
+        if(tarefa.trim() !== ""){ // se tarefa for diferente de vazio...
+            setListaTarefas([...listaTarefas, tarefa]);
+            setTarefa(""); //limpa o input
+        }
     }
 
     return(
         <div>
             <h1>Mostrar Tarefas</h1>
 
-            <input type="text" placeholder="Digite aqui" onChange={mudarTarefa} value={tarefa} />
-            <p>Sua tarefa: {tarefa}</p>
+            <input type="text" placeholder="Digite aqui" onChange={(e) => setTarefa(e.target.value)} value={tarefa} />
+            
+            <button onClick={adicionarTarefa} disabled={tarefa.trim() === ""}> {/*disabled -> Evita que o botão funcione se o input estiver vazio*/}
+                Adicionar Tarefa
+            </button>
+
+            <ul>
+                {listaTarefas.map((tarefass, index) =>
+                    <li key = {index}>{tarefass}</li>
+                )}
+            </ul>
+
         </div>
     );
 }
